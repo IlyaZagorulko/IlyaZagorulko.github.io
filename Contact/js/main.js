ba6f2c74e5b2b882f682fc5b7565968e73d93a48
IlyaZagorulko <46575183+IlyaZagorulko@users.noreply.github.com>
2019-05-16 17:26:04 +0300

    Add files via upload

---
+++ Contact/js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+	 $('.our-team').slick({
+   infinite: true,
+   slidesToShow: 4,
+   slidesToScroll: 1
+  });
+	$('.feedback').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1
+ });
+
+
+
+
+});
